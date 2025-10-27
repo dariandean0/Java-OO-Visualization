65e1cfb6adad34486ff53755035e37471ba5f6ef
Korgen1 <joshkal9876@hotmail.com>
2025-10-26 22:32:47 -0400

    Emscripten compatibility

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -67,7 +67,6 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 use std::slice;
 use std::str;
-use crate::{execution_flow_gen, no_flow_gen, visualizer};
 
 fn ptr_to_str<'a>(ptr: *const u8, len: usize) -> &'a str {
     let bytes = unsafe { slice::from_raw_parts(ptr, len) };
@@ -77,22 +76,22 @@ fn to_c_string(s: String) -> *mut c_char {
     CString::new(s).unwrap().into_raw()
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn wasm_execution_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let vec = execution_flow_gen(java_code);
     let json = serde_json::to_string(&vec).unwrap_or_else(|e| format!("Error serializing: {}", e));
-    to_c_string(json);
+    to_c_string(json)
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn wasm_no_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let result = no_flow_gen(java_code);
     to_c_string(result)
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn wasm_visualize_java_code(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let result = match visualizer::visualize_java_code(java_code) {
