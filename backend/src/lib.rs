4ff7c1489f68f588d3314334488b4e600222859f
Korgen1 <joshkal9876@hotmail.com>
2025-10-27 07:32:58 -0400

    Emscripten compatibility

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -77,7 +77,7 @@ fn to_c_string(s: String) -> *mut c_char {
 }
 
 #[no_mangle]
-pub extern "C" fn wasm_execution_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
+pub unsafe extern "C" fn wasm_execution_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let vec = execution_flow_gen(java_code);
     let json = serde_json::to_string(&vec).unwrap_or_else(|e| format!("Error serializing: {}", e));
@@ -85,14 +85,14 @@ pub extern "C" fn wasm_execution_flow_gen(ptr: *const u8, len: usize) -> *mut c_
 }
 
 #[no_mangle]
-pub extern "C" fn wasm_no_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
+pub unsafe extern "C" fn wasm_no_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let result = no_flow_gen(java_code);
     to_c_string(result)
 }
 
 #[no_mangle]
-pub extern "C" fn wasm_visualize_java_code(ptr: *const u8, len: usize) -> *mut c_char {
+pub unsafe extern "C" fn wasm_visualize_java_code(ptr: *const u8, len: usize) -> *mut c_char {
     let java_code = ptr_to_str(ptr, len);
     let result = match visualizer::visualize_java_code(java_code) {
         Ok(output) => output,
