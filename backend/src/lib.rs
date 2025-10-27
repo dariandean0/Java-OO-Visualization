ed61c75f5cda467b216576c34ffab608e8b0ef2f
Korgen1 <joshkal9876@hotmail.com>
2025-10-27 09:58:10 -0400

    WASM functionality to index

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -92,8 +92,11 @@ pub extern "C" fn wasm_no_flow_gen(ptr: *const u8, len: usize) -> *mut c_char {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn wasm_visualize_java_code(ptr: *const u8, len: usize) -> *mut c_char {
-    let java_code = ptr_to_str(ptr, len);
+pub extern "C" fn wasm_visualize_java_code(ptr: *const c_char) -> *mut c_char {
+    //let java_code = ptr_to_str(ptr, len);
+
+    let c_str = unsafe { std::ffi::CStr::from_ptr(ptr) };
+    let java_code = c_str.to_str().unwrap_or("");
     eprintln!("Received Java code: {:?}", java_code);
     let result = match visualizer::visualize_java_code(java_code) {
         Ok(output) => output,
