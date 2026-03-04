30f5b2a667a2c6ff161fe1610b66bb90ccd89cde
Korgen1 <joshkal9876@hotmail.com>
2026-03-04 08:54:26 -0500

    Undo last commit

--- frontend/javascript/visualizer.js
+++ frontend/javascript/visualizer.js
@@ -35,8 +35,8 @@ import * as Viz from 'https://cdn.jsdelivr.net/npm/@viz-js/viz@3.20.0/+esm'
     }
 
     async function update() {
-        //var dotCode = wasmVisualizeJavaCode(getEditorContent())
-        var dotCode = wasmExecFlowGen(getEditorContent());
+        var dotCode = wasmVisualizeJavaCode(getEditorContent())
+        //var dotCode = wasmExecFlowGen(getEditorContent());
         console.log(dotCode);
 
         Viz.instance().then(viz => {
