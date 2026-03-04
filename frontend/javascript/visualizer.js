07caa9d6272d850da296038c7e88e5505a54a104
Korgen1 <joshkal9876@hotmail.com>
2026-03-04 08:38:34 -0500

    fix

--- frontend/javascript/visualizer.js
+++ frontend/javascript/visualizer.js
@@ -36,7 +36,7 @@ import * as Viz from 'https://cdn.jsdelivr.net/npm/@viz-js/viz@3.20.0/+esm'
 
     async function update() {
         //var dotCode = wasmVisualizeJavaCode(getEditorContent())
-        var dotCode = wasmExecFlowGen(getEditorContent);
+        var dotCode = wasmExecFlowGen(getEditorContent());
         console.log(dotCode);
 
         Viz.instance().then(viz => {
